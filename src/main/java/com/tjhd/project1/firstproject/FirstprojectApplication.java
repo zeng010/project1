9bff2e0c6b2bbcd96fffd00db0fe3e427630bd4f
zeng010 <zeng_h12@163.com>
2021-10-12 10:41:50 +0800

    first commit

---
+++ src/main/java/com/tjhd/project1/firstproject/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tjhd.project1.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstprojectApplication.class, args);
+    }
+
+}
