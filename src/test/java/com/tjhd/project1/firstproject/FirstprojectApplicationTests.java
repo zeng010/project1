57c519ca72dadc323a8f792c6fed4a4e4a73e8ea
zeng010 <zeng_h12@163.com>
2021-10-17 15:20:21 +0800

    新增shiro

--- src/test/java/com/tjhd/project1/firstproject/FirstprojectApplicationTests.java
+++ src/test/java/com/tjhd/project1/firstproject/FirstprojectApplicationTests.java
@@ -1,13 +1,55 @@
 package com.tjhd.project1.firstproject;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tjhd.project1.firstproject.bean.Blog;
+import com.tjhd.project1.firstproject.bean.User;
+import com.tjhd.project1.firstproject.mapper.BlogMapper;
+import com.tjhd.project1.firstproject.mapper.UserMapper;
+import com.tjhd.project1.firstproject.service.impl.BlogServiceImpl;
+import com.tjhd.project1.firstproject.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FirstprojectApplicationTests {
 
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    UserServiceImpl userService;
+    @Autowired
+    BlogServiceImpl blogService;
+    @Autowired
+    BlogMapper blogMapper;
+
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void test1(){
+        System.out.println("hello world");
+    }
+
+    @Test
+    void test2(){
+        User user = userMapper.selectOne(null);
+        System.out.println(user);
+        User one = userService.getOne(null);
+        System.out.println("---->"+one);
+        Blog one1 = blogService.getOne(null);
+        System.out.println("========>>>>"+one1);
+    }
+
+    @Test
+    void test3(){
+        Page<Blog> page = new Page<>(2,1);
+        Page<Blog> page1 = blogMapper.selectPage(page, null);
+        System.out.println("总数据：--->"+page1.getTotal());
+        System.out.println("记录为：--->"+page.getRecords());
+
+        Page<Blog> page2 = blogService.page(page, null);
+        System.out.println("service查询的分页数据为：--->"+page2.getRecords());
+    }
 }
